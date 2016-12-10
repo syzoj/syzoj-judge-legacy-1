218f058fb4aea0f9435ac32153fa3ef2cd8c1b6b
Menci <HuangHaoRui301@Gmail.com>
2016-12-10 15:28:04 +0800

    Add VB and C++11 support

---
+++ languages/cpp11.js
@@ -0,0 +1,45 @@
+let Promise = require('bluebird');
+let path = require('path');
+let child_process = Promise.promisifyAll(require('child_process'));
+let fs = Promise.promisifyAll(require('fs'));
+
+async function isFile(file) {
+  try {
+    let stat = await fs.statAsync(file);
+    return stat.isFile();
+  } catch (e) {
+    return false;
+  }
+}
+
+module.exports = {
+  minOutputLimit: 1024,
+  minProcessLimit: 1,
+  getFilename(file) {
+    return file + '.cpp';
+  },
+  async compile(file) {
+    let parsed = path.parse(file)
+    let execFile = path.join(parsed.dir, parsed.name);
+
+    if (await isFile(execFile)) {
+      await fs.unlinkAsync(execFile);
+    }
+
+    let output;
+
+    try {
+      output = await child_process.execAsync(`g++ ${file} -o ${execFile} -O2 -lm -static -DONLINE_JUDGE -std=c++11 -fdiagnostics-color=always 2>&1 || true`, {
+        timeout: 5000
+      });
+    } catch (e) {
+      output = 'Time limit exceeded while compiling';
+    }
+
+    return {
+      success: await isFile(execFile),
+      execFile: execFile,
+      output: output
+    };
+  }
+};
---
+++ languages/vbnet.js
@@ -0,0 +1,48 @@
+let Promise = require('bluebird');
+let path = require('path');
+let child_process = Promise.promisifyAll(require('child_process'));
+let fs = Promise.promisifyAll(require('fs'));
+
+async function isFile(file) {
+  try {
+    let stat = await fs.statAsync(file);
+    return stat.isFile();
+  } catch (e) {
+    return false;
+  }
+}
+
+module.exports = {
+  minOutputLimit: 10240,
+  minProcessLimit: 2,
+  getFilename(file) {
+    return file + '.vb';
+  },
+  async compile(file) {
+    let parsed = path.parse(file)
+    let execFile = path.join(parsed.dir, parsed.name);
+
+    if (await isFile(execFile)) {
+      await fs.unlinkAsync(execFile);
+    }
+
+    let output;
+
+    try {
+      output = await child_process.execAsync(`vbnc ${file} -define:ONLINE_JUDGE 2>&1 || true`, {
+        timeout: 5000
+      });
+      output += await child_process.execAsync(`mkbundle ${execFile}.exe -o ${execFile} 2>&1 || true`, {
+        timeout: 5000
+      });
+    } catch (e) {
+      output = 'Time limit exceeded while compiling';
+    }
+
+    return {
+      success: await isFile(execFile),
+      execFile: execFile,
+      output: output
+    };
+  }
+};
